11b076d5c6c68e7214876b5cc5d2ae15815cbf77
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-01-05 13:12:49 +0100

    no necesitamos quotes en el resto del objeto

--- services/groq.ts
+++ services/groq.ts
@@ -10,13 +10,13 @@ export const groqService: AIService = {
     async chat(messages: ChatMessage[]) {
         const chatCompletion = await groq.chat.completions.create({
             messages,
-            "model": "openai/gpt-oss-120b",
-            "temperature": 1,
-            "max_completion_tokens": 8192,
-            "top_p": 1,
-            "stream": true,
-            "reasoning_effort": "medium",
-            "stop": null
+            model: "openai/gpt-oss-120b",
+            temperature: 1,
+            max_completion_tokens: 8192,
+            top_p: 1,
+            stream: true,
+            reasoning_effort: "medium",
+            stop: null
         });
 
         return async function* () {
