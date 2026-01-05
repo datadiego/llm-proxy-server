b73c292e0f5e3c6df089ad16efb0a8acee523017
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-01-05 13:07:37 +0100

    adaptamos el script a nuestros tipos

--- services/groq.ts
+++ services/groq.ts
@@ -1,26 +1,29 @@
 import { Groq } from 'groq-sdk';
+import type { AIService, ChatMessage } from '../types';
 const API_KEY = process.env.GROQ_API_KEY;
 const groq = new Groq({
-  apiKey: API_KEY
+    apiKey: API_KEY
 });
 
-const chatCompletion = await groq.chat.completions.create({
-  "messages": [
-    {
-      "role": "user",
-      "content": "que es una funcion anonima en javascript?"
+export const groqService: AIService = {
+    name: 'Groq',
+    async chat(messages: ChatMessage[]) {
+        const chatCompletion = await groq.chat.completions.create({
+            messages,
+            "model": "openai/gpt-oss-120b",
+            "temperature": 1,
+            "max_completion_tokens": 8192,
+            "top_p": 1,
+            "stream": true,
+            "reasoning_effort": "medium",
+            "stop": null
+        });
+
+        return async function* () {
+            for await (const chunk of chatCompletion) {
+                yield chunk.choices[0]?.delta?.content || '';
+            }
+        }();
     }
-  ],
-  "model": "openai/gpt-oss-120b",
-  "temperature": 1,
-  "max_completion_tokens": 8192,
-  "top_p": 1,
-  "stream": true,
-  "reasoning_effort": "medium",
-  "stop": null
-});
-
-for await (const chunk of chatCompletion) {
-  process.stdout.write(chunk.choices[0]?.delta?.content || '');
 }
 
