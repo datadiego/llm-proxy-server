527078eb6f1f9fea3bcf8dceb01a354539c37ca7
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-01-06 13:35:31 +0100

    usamos los tipos creados

--- services/gemini.ts
+++ services/gemini.ts
@@ -1,46 +1,50 @@
-import { AIService, ChatMessage } from '../types';
+import type { AIService, ChatMessage } from '../types';
 
 import {
   GoogleGenAI,
 } from '@google/genai';
 
-async function main() {
-  const ai = new GoogleGenAI({
-    apiKey: process.env.GEMINI_API_KEY,
-  });
-  const tools = [
-    {
-      googleSearch: {
-      }
-    },
-  ];
-  const config = {
-    thinkingConfig: {
-      thinkingBudget: 0,
-    },
-    tools,
-  };
-  const model = 'gemini-flash-lite-latest';
-  const contents = [
-    {
-      role: 'user',
-      parts: [
-        {
-          text: `que es una funcion anonima en javascript?`,
-        },
-      ],
-    },
-  ];
-
-  const response = await ai.models.generateContentStream({
-    model,
-    config,
-    contents,
-  });
-  let fileIndex = 0;
-  for await (const chunk of response) {
-    console.log(chunk.text);
-  }
+export const geminiService: AIService = {
+    name: 'Gemini',
+    async chat(messages: ChatMessage[]) {
+        return async function* () {
+          const ai = new GoogleGenAI({
+            apiKey: process.env.GEMINI_API_KEY,
+          });
+          const tools = [
+            {
+              googleSearch: {
+              }
+            },
+          ];
+          const config = {
+            thinkingConfig: {
+              thinkingBudget: 0,
+            },
+            tools,
+          };
+          const model = 'gemini-flash-lite-latest';
+          const contents = messages.map((msg) => ({
+            role: msg.role,
+            parts: [
+              {
+                text: msg.content,
+              },
+            ],
+          }));
+        
+          const response = await ai.models.generateContentStream({
+            model,
+            config,
+            contents,
+          });
+        
+          for await (const chunk of response) {
+            if (typeof chunk.text === 'string') {
+              yield chunk.text;
+            }
+          }
+        }();
+    }
 }
 
-main();
