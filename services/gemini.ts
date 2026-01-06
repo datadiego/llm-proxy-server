10487b7377d4119ff2cb9cd9ebbe94920cedde2b
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-01-06 13:30:20 +0100

    añadimos el script basico para gemini

---
+++ services/gemini.ts
@@ -0,0 +1,44 @@
+import {
+  GoogleGenAI,
+} from '@google/genai';
+
+async function main() {
+  const ai = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY,
+  });
+  const tools = [
+    {
+      googleSearch: {
+      }
+    },
+  ];
+  const config = {
+    thinkingConfig: {
+      thinkingBudget: 0,
+    },
+    tools,
+  };
+  const model = 'gemini-flash-lite-latest';
+  const contents = [
+    {
+      role: 'user',
+      parts: [
+        {
+          text: `que es una funcion anonima en javascript?`,
+        },
+      ],
+    },
+  ];
+
+  const response = await ai.models.generateContentStream({
+    model,
+    config,
+    contents,
+  });
+  let fileIndex = 0;
+  for await (const chunk of response) {
+    console.log(chunk.text);
+  }
+}
+
+main();
