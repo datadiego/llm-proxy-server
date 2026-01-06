c6730d197d826d651e2e3cce47e37da286c2f091
Juan Diego Mariscal Millan <juandiegomariscal@gmail.com>
2026-01-06 13:01:36 +0100

    dejamos mandar un mensaje personalizado al test

--- test.sh
+++ test.sh
@@ -1,4 +1,13 @@
+
 #!/bin/bash
+
+MENSAJE="$1"
+
+if [ -z "$MENSAJE" ]; then
+  echo "Uso: $0 \"mensaje a enviar\""
+  exit 1
+fi
+
 http POST http://localhost:3000/chat \
   Content-Type:application/json \
-  'messages:=[{"role":"user","content":"¿Que es una funcion anonima en javascript?"}]'
+  "messages:=[{\"role\":\"user\",\"content\":\"$MENSAJE\"}]"
